080e56b0fea04e22926a3947c1ddeed35e677672
= <=>
2023-11-10 11:00:53 -0600

    REDISEÑO DE NAVBAR Y NAVLINK AGREGANDO EL LOGO COCO, CAMBIO DE COLOR Y ESTILO.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,18 +4,31 @@ import { useSelector } from 'react-redux'
 
 const Navbar = () => {
     const state = useSelector(state => state.handleCart)
+
+    const estiloAmarillo = {
+        backgroundColor: '#FFD700',
+        color: '#000', // Cambia el color del texto según sea necesario
+        
+    };
+
+    const estiloRosa = {
+        backgroundColor: '#8C60AB',
+        color: '#000', // Cambia el color del texto según sea necesario
+    };
+
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light py-3 sticky-top">
+        <nav className="navbar navbar-expand-lg navbar-dark py-1 sticky-top" style={estiloRosa}>
             <div className="container">
-                <NavLink className="navbar-brand fw-bold fs-4 px-2" to="/">COSITAS</NavLink>
-                <button className="navbar-toggler mx-2" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <NavLink to="/" className="navbar-brand fw-bold fs-3 px-1">
+                    <img src="./assets/coco-logo.png" alt="Logo" className="logo-img" style={{ width: '80px', marginRight: '1px' }}/> COSITAS
+                </NavLink>                <button className="navbar-toggler mx-2" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
 
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav m-auto my-2 text-center">
+                    <ul className="navbar-nav m-auto my-1 text-center">
                         <li className="nav-item">
-                            <NavLink className="nav-link" to="/">Inicio </NavLink>
+                            <NavLink className="nav-link" to="/">Inicio</NavLink>
                         </li>
                         <li className="nav-item">
                             <NavLink className="nav-link" to="/product">Top</NavLink>
@@ -28,9 +41,9 @@ const Navbar = () => {
                         </li>
                     </ul>
                     <div className="buttons text-center">
-                        <NavLink to="/login" className="btn btn-outline-dark m-2"><i className="fa fa-sign-in-alt mr-1"></i> Iniciar Sesión</NavLink>
-                        <NavLink to="/register" className="btn btn-outline-dark m-2"><i className="fa fa-user-plus mr-1"></i> Registrarte</NavLink>
-                        <NavLink to="/cart" className="btn btn-outline-dark m-2"><i className="fa fa-cart-shopping mr-1"></i> Cositas ({state.length}) </NavLink>
+                        <NavLink to="/login" className="btn btn-outline-light m-2"><i className="fa fa-sign-in-alt mr-1"></i> Iniciar Sesión</NavLink>
+                        <NavLink to="/register" className="btn btn-outline-light m-2"><i className="fa fa-user-plus mr-1"></i> Registrarte</NavLink>
+                        <NavLink to="/cart" className="btn btn-outline-light m-2"><i className="fa fa-cart-shopping mr-1"></i> Cositas ({state.length}) </NavLink>
                     </div>
                 </div>
 
--- src/components/main.jsx
+++ src/components/main.jsx
@@ -7,13 +7,13 @@ const Home = () => {
     <div className="hero border-1 pb-3">
       <div className="card bg-dark text-white border-0 mx-3 position-relative">
         <img
-          className="card-img img-fluid w-100 max-height-300 object-fit-cover"
-          src="./assets/main.png.jpg"
+          className="card-img img-fluid w-100 object-fit-cover"
+          src="./assets/main_nun.jpg"
           alt="Card"
         />
         <div className="card-img-overlay d-flex align-items-center">
           <div className="container text-center text-white">
-            <h5 className="card-title fs-1 text fw-lighter">C O S I T A S</h5>
+            <h5 className="card-title fs-1 text fw-lighter">C O S I T O S</h5>
             <p className="card-text fs-5 d-none d-sm-block">
               El proyecto no solo ha sido una oportunidad para impulsar la
               economía de los artesanos, sino también un recordatorio constante
